985a37a803b7fc59a9c61c21daf1b100c6abe7ff
Guanglei Liu <guanglei1025@gmail.com>
2021-09-15 10:18:26 -0400

    added OrderRow

--- Shared/View/ShoppingCartView/ShoppingCartView.swift
+++ Shared/View/ShoppingCartView/ShoppingCartView.swift
@@ -36,6 +36,9 @@ struct ShoppingCartView: View {
                             .padding(.trailing)
                     }
                 }
+                .toolbar {
+                    EditButton()
+                }
                 .navigationTitle("Cart")
             }
         }
